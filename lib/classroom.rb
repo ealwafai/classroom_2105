64abac71b5a35cd3d111f671d792673181af61fa
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-05-12 15:54:42 -0600

    Update lib

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,30 @@
 class Classroom
+
+  attr_reader :subject, :capacity, :students
+
+  def initialize(subject, capacity)
+    @subject = subject
+    @capacity = capacity
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+  end
+
+  def yell_at_students(student)
+    
+  end
+
+  def over_capacity
+    if @capacity > 4
+      true
+    else
+      false
+    end
+  end
+
+  def kick_out
+    @students.pop
+  end
 end
